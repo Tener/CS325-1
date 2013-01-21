1ea86127fbab252fa034aafc3d04882f1240ea88
josephbudin <jpbudi11@smumn.edu>
2013-01-21 17:08:14 -0600

    Safe TDD

---
+++ CS325/src/assignments/safe/SafeImpl.java
@@ -0,0 +1,35 @@
+package assignments.safe;
+
+public class SafeImpl implements Safe {
+
+
+	private char[] display = new char[Safe.DISPLAY_LENGTH];
+	private int index = 0;
+	private boolean locked = true;
+	
+	public SafeImpl() {
+		display = Safe.BLANK_DISPLAY.toCharArray();
+	}
+
+	@Override
+	public boolean isLocked() {
+		return locked;
+	}
+
+	@Override
+	public Object readDisplay() {
+		return String.valueOf(display);
+	}
+
+	@Override
+	public void enter(char c) {
+		if (Character.isDigit(c)){
+			display [index++] = c;
+		}
+		if (String.valueOf(display).equals("123456")){
+			locked = false;
+			display = Safe.OPEN_DISPLAY.toCharArray();
+		}
+	}
+
+}
